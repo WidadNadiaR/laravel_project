84e63980dcaa3e69aa399aaf19cb600050afcf48
WidadNadiaR <widadnadia125@gmail.com>
2025-02-10 11:22:37 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
-
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view(view: 'home');
+});
+
+//localhost:8000/admin/inputbuku
+Route::get('/admin/inputbuku', function () {
+    return view(view: 'admin.inputbuku');
+});
+
+// untuk kategori
+// Route::get('/admin/kategori', function () {
+//     return view(view: 'admin.kategori');
+// });
+
+Route::get ('/admin/kategori', [CategoryController::class,'index'])
+                                     ->name('admin.inkategori');
+//untuk penulis
+Route::get('/admin/penulis', function () {
+    return view(view: 'admin.penulis');
 });
